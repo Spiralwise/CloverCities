29aa013ff036d7f51da6542b067ccd9757f1d358
Spiralwise <contact@spiralwise.net>
2017-04-19 16:23:40 +0200

    Corner style building

--- Assets/Scripts/BuildingGenerator.cs
+++ Assets/Scripts/BuildingGenerator.cs
@@ -9,7 +9,7 @@ public class BuildingGenerator : MonoBehaviour {
 	public int maxStack = 3;
 	[Range(0f, 1f)]
 	public float stackVariation = 0.5f;
-	public float heightVariation = 0.8f;
+	public float heightGrowth = 0.8f;
 
 	const int faces = 5; // Do not do bottom face
 	const int ring = 8;
@@ -19,6 +19,13 @@ public class BuildingGenerator : MonoBehaviour {
 	enum StackMode {
 		OnTop,
 		Neighbor
+	}
+
+	enum Corner {
+		NorthWest,
+		NorthEast,
+		SouthEast,
+		SouthWest
 	}
 
 	public float RandomVariation () {
@@ -53,7 +60,8 @@ public class BuildingGenerator : MonoBehaviour {
 		Vector3[] vertices = new Vector3[verticesByStack * stack];
 		int[] triangles = new int[faces * 6 * stack];
 
-		CreateAsOnTop (vertices, triangles, size, stack);
+		//CreateAsOnTop (vertices, triangles, size, stack);
+		CreateAsCorner (vertices, triangles, size, stack);
 
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
@@ -62,9 +70,9 @@ public class BuildingGenerator : MonoBehaviour {
 
 	/* Create a building mesh that stacks cube on each other.
 	 */
-	void CreateAsOnTop(Vector3[] vertices, int[] triangles, Vector3 size, int stack){
+	void CreateAsOnTop(Vector3[] vertices, int[] triangles, Vector3 size, int stack) {
 		float currentHeight = 0f;
-		float nextHeight = size.y;
+		float nextHeight = size.y; // TODO Not good, how can we now the final size?
 		Rect surface = new Rect(-size.x/2, -size.z/2, size.x, size.z);
 
 		int v = 0;
@@ -92,12 +100,59 @@ public class BuildingGenerator : MonoBehaviour {
 
 			if (s < stack-1) {
 				currentHeight += nextHeight;
-				nextHeight *= Random.Range (heightVariation, 1f);
+				nextHeight *= Random.Range (heightGrowth, 1f);
 				surface.xMin += RandomVariation ();
 				surface.xMax -= RandomVariation ();
 				surface.yMin += RandomVariation ();
 				surface.yMax -= RandomVariation ();
 			}
+		}
+	}
+
+	/* Create a building mesh that place stacks on the corners.
+	 */
+	void CreateAsCorner(Vector3[] vertices, int[] triangles, Vector3 size, int stack) {
+		Vector3 halfSize = size / 2f;
+		Corner corner = (Corner)Random.Range (0, 4);
+
+		int v = 0;
+		for (int s = 0; s < stack; s++) { // TODO Redundancy with CreateAsOnTop(...), make a function
+			Rect surface = new Rect();
+			Vector3 dimension = new Vector3 (
+				                    size.x * (1f + RandomVariation ()) / 2, 
+									Mathf.Pow ((float)(s + 1) / (stack + 1), heightGrowth) * size.y,
+				                    size.z * (1f + RandomVariation ()) / 2);
+			if (corner == Corner.NorthWest)
+				surface = new Rect (-halfSize.x, halfSize.z, dimension.x, dimension.z);
+			else if (corner == Corner.NorthEast)
+				surface = new Rect (halfSize.x - dimension.x, halfSize.z, dimension.x, dimension.z);
+			else if (corner == Corner.SouthEast)
+				surface = new Rect (halfSize.x - dimension.x, dimension.z - halfSize.z, dimension.x, dimension.z);
+			else if (corner == Corner.SouthWest)
+				surface = new Rect (-halfSize.x, dimension.z - halfSize.z, dimension.x, dimension.z);
+
+			int currentV;
+			int offsetV = s * verticesByStack;
+			for (int t = 0; t < 2; t++) {
+				int onTop = t % 2;
+				currentV = ring * t + offsetV;
+				vertices [currentV] = vertices [currentV + 7] = new Vector3 (surface.x, onTop * dimension.y, surface.y);
+				vertices [currentV + 1] = vertices [currentV + 2] = new Vector3 (surface.xMax, onTop * dimension.y, surface.y);
+				vertices [currentV + 3] = vertices [currentV + 4] = new Vector3 (surface.xMax, onTop * dimension.y, surface.yMax);
+				vertices [currentV + 5] = vertices [currentV + 6] = new Vector3 (surface.x, onTop * dimension.y, surface.yMax);
+			}
+			for (int i = 0; i < 4; i++)
+				vertices [top + i + offsetV] = vertices [ring + i * 2 + offsetV];
+
+			int x;
+			for (x = offsetV; x < 6 + offsetV; x+=2)
+				v = SetQuad (triangles, v, x, x + 1, x + ring, x + ring + 1);
+			x = 6 + offsetV;
+			v = SetQuad (triangles, v, x, x + 1, x + 8, x + 9);
+			x = top + offsetV;
+			v = SetQuad (triangles, v, x, x + 1, x + 3, x + 2);
+
+			corner = (corner == (Corner)3) ? 0 : corner + 1;
 		}
 	}
 }
